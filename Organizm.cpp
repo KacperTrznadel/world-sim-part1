a86ab290f58491133ff96b4d14efc5f310924454
Kacper Trznadel <kacper.trznadel2005@gmail.com>
2025-04-17 20:09:35 +0200

    Created Organizm.cpp - now ready for Zwierze subclasses

---
+++ Organizm.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "Organizm.h"
+
+Organizm::Organizm(Swiat* swiat, int sila, int inicjatywa, int x, int y)
+    : swiat(swiat), sila(sila), inicjatywa(inicjatywa), x(x), y(y) {}
+Organizm::~Organizm() {}
+int Organizm::getSila() const {
+    return sila;
+}
+int Organizm::getInicjatywa() const {
+    return inicjatywa;
+}
+int Organizm::getX() const {
+    return x;
+}
+int Organizm::getY() const {
+    return y;
+}
+int Organizm::getWiek() const {
+    return wiek;
+}
+bool Organizm::czyZywy() const {
+    return zywy;
+}
+void Organizm::setSila(int sila) {
+    this->sila = sila;
+}
+void Organizm::setPozycja(int newX, int newY) {
+    x = newX;
+    y = newY;
+}
+void Organizm::wiekInkrementacja() {
+    wiek++;
+}
+void Organizm::zabij() {
+    zywy = false;
+}
